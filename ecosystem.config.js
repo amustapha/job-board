c38d1f6b83daf6e60eb8be5f98ab58405cf713c4
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2025-04-08 00:17:38 +0100

    create pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "job-board",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "400M",
+      restart_delay: 100,
+      exp_backoff_restart_delay: 100,
+    },
+    {
+      name: "job-updater",
+      script: "node",
+      args: "dist/scripts/index.js ",
+      env: {
+        NODE_ENV: "production",
+      },
+      cron_restart: "0 0 * * *", // Run at midnight every day
+      autorestart: false,
+      max_memory_restart: "500M",
+    },
+  ],
+};
